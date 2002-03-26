827842824281d6933c7a33aa5be28a45995bf50c
Arjun Sanyal <arjun@openforce.net>
2002-03-26 21:31:01 +0000

    use yon's new list_of_ns_sets stuff

--- www/dotlrn-members-portlet.tcl
+++ www/dotlrn-members-portlet.tcl
@@ -19,18 +19,8 @@ set admin_p [dotlrn::user_can_admin_community_p -user_id $user_id $community_id]
 set read_private_data_p [dotlrn::user_can_read_private_data_p $user_id]
 
 # Get all users for this community, including role
-set list_of_users [dotlrn_community::list_users $community_id]
-
-template::multirow create users rel_id rel_type user_id first_names last_name email
-
-foreach user $list_of_users {
-    template::multirow append users \
-        [lindex $user 0] \
-        [dotlrn_community::get_role_pretty_name_from_rel_type -rel_type [lindex $user 1]] \
-        [lindex $user 2] \
-        [lindex $user 3] \
-        [lindex $user 4] \
-        [lindex $user 5]
-}
+template::util::list_of_ns_sets_to_multirow \
+    -rows [dotlrn_community::list_users $community_id] \
+    -var_name "users"
 
 ad_return_template
