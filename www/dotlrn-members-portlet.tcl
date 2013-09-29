bec1ddbeec7dbe0735f21c002aee268669ccd818
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2013-09-29 13:56:47 +0000

    - replace deprecated "ad_get_user_id" by "ad_conn user_id"

--- www/dotlrn-members-portlet.tcl
+++ www/dotlrn-members-portlet.tcl
@@ -33,7 +33,7 @@ set return_url [ad_return_url]
 
 set admin_p [dotlrn::user_can_admin_community_p -user_id $user_id -community_id $community_id]
 set read_private_data_p [dotlrn::user_can_read_private_data_p -user_id $user_id -object_id $community_id]
-set spam_p [dotlrn::user_can_spam_community_p -user_id [ad_get_user_id] -community_id $community_id]
+set spam_p [dotlrn::user_can_spam_community_p -user_id [ad_conn user_id] -community_id $community_id]
 # Get all users for this community, including role
 template::util::list_of_ns_sets_to_multirow \
     -rows [dotlrn_community::list_users $community_id] \
